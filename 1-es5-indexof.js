1e85f2bee2f2b521c0ea53c9a6331ff3c40b4b3d
imeTynn <37203610+imeTynn@users.noreply.github.com>
2018-12-03 10:11:21 +0800

    Create 1-es5-indexof.js

---
+++ 1-es5-indexof.js
@@ -0,0 +1,14 @@
+ function indexof(arr,ele,starti){
+      if(starti===undefined){
+        starti=0;
+      }
+      for(var i=starti;i<arr.length;i++){
+        if(ele==arr[i]){
+          return i;
+        }else{
+          return -1;
+        }
+      }
+    }
+    var arr=[1,34,65,45,3,5];
+    console.log(indexof(arr,3));
