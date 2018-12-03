245182c984135a6ede027fa229b8902294011516
imeTynn <37203610+imeTynn@users.noreply.github.com>
2018-12-03 10:30:31 +0800

    Create es6-剩余参数.js

---
+++ es6-剩余参数.js
@@ -0,0 +1,10 @@
+//计算工资
+function calc(ename,...salary){
+console.log(`${ename}的总工资为${
+    salary.reduce(function(prev,elem){
+       return pre+elem;
+      },0)
+ }`)
+}
+calc('lili',123,343,4);
+calc('mm',34,56,67）;
